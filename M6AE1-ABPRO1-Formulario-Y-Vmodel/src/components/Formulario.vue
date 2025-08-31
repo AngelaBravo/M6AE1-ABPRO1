9a7cdff5c066db0817d7177086ebe90a7ae268c5
Angela Bravo <angelabravop@gmail.com>
2025-08-30 21:17:26 -0400

    solo ordene sigue faltando el resumen despues del submit

--- M6AE1-ABPRO1-Formulario-Y-Vmodel/src/components/Formulario.vue
+++ M6AE1-ABPRO1-Formulario-Y-Vmodel/src/components/Formulario.vue
@@ -1,21 +1,16 @@
 <template>
     <form>
         <input v-model="nombre" type="text" class="form-control mb-3" placeholder="Nombre">
-        <!-- <P>Tu nombre es: {{ nombre }}</P> -->
-
-        <div class="mb-3">
-            <!-- <label for="exampleInputEmail1" class="form-label">Email address</label> -->
-            <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="E-mail">
-            <!-- <div id="emailHelp" class="form-text">Tu email es: {{ email }}</div> -->
-        </div>
+        <input v-model="email" type="email" class="form-control mb-3" id="exampleInputEl1" placeholder="E-mail">
         <input v-model="edad" type="text" class="form-control mb-3" placeholder="Edad">
-        <!-- <P>Tu edad es: {{ edad }}</P> -->
         <button type="submit" class="btn btn-primary">Submit</button>
-
-        <h3 class="mt-4" >Resumen</h3>
-        <p>Nombre: {{ nombre }}</p>
-        <p>Email: {{ email }}</p>
-        <p>Edad: {{ edad }}</p>
+        
+        <slot name="resumen">
+            <h3 class="mt-4" >Resumen</h3>
+            <p>Nombre: {{ nombre }}</p>
+            <p>Email: {{ email }}</p>
+            <p>Edad: {{ edad }}</p>
+        </slot>
 
     </form>
 </template>
